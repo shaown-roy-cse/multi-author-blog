16b25a0bcdd1858d0bc2f4f99e4d2bebf0e6a4d5
shaown-roy-cse <shaown307085@gmail.com>
2026-07-27 23:10:03 +0600

    Fix media image configuration

--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -136,6 +136,4 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/login/'
-MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'post_images'
--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
@@ -31,10 +32,6 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
-]
 
 
 # Media files (Images)
